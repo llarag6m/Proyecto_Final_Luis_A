70c52c65700b8983692c53b231edff81e36ce3d2
Luis Alberto <l.al98@hotmail.com>
2023-07-12 16:32:01 -0600

    Proyecto final agregado y corrigiendo

--- assets/js/components/darkMode.js
+++ assets/js/components/darkMode.js
@@ -1,8 +1,24 @@
+const ls = window.localStorage
+
 function darkMode (){
     const btn = document.getElementById('btn--dark')
 
+    if(ls.getItem('color') === 'dark'){
+        document.body.classList.add('dark')
+    }else{
+        document.body.classList.remove('dark')
+    }
+
+
     btn.addEventListener('click', () =>{
-        document.body.classList.toggle('dark--mode')
+        document.body.classList.toggle('dark')
+
+        if(document.body.classList.contains('dark')){
+            ls.setItem('color', 'dark')
+        }else{
+            ls.removeItem('color')
+            
+        }
     })
     
 }
